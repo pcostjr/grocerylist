0d0d7792bb10bd1b56a1d6336df89ba9d945ef16
Peter Costantino <pcostantino@acitech.org>
2024-09-25 09:53:16 -0400

    reversion

--- grocerylist.py
+++ grocerylist.py
@@ -3,9 +3,3 @@
 # author:
 # created:
 # last update:
-
-<<<<<<< HEAD
-# this comment wasn't here before!
-# this comment was made on a  new branch
-=======
->>>>>>> parent of 8513b4c (Added test comment)
